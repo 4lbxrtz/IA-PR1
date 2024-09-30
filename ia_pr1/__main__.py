2173bddb203f232961c85d7ef91ea1dd93a87739
4lbxrtz <4lbxrtz@gmail.com>
2024-09-30 11:15:44 +0100

    feat: first commit

---
+++ ia_pr1/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for the application."""
+
+from ia_pr1.main import main
+
+if __name__ == "__main__":
+    main()
---
+++ ia_pr1/main.py
@@ -0,0 +1,6 @@
+"""Main module for the application."""
+
+
+def main():
+    """Entry point function for the application."""
+    print("Hello, world!")
